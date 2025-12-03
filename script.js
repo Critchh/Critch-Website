b9ac383913dece8edf3f19ea6c1d6f116f723796
Critchh <christianpol.valles@gmail.com>
2025-12-03 11:49:04 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function updateClock() {
+  const now = new Date();
+
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+  };
+
+  const clockElement = document.getElementById("liveClock");
+  if (clockElement) {
+    clockElement.textContent = now.toLocaleString("en-US", options);
+  }
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
+
+
+
+
